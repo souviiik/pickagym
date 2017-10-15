28678c15ad88f84ba781e67cbb8694ac17e44275
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2017-10-15 21:24:43 +0530

    import promise from 'redux-promise'
    import promise from 'redux-promise'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import Header from './components/common/header';
 import Footer from './components/common/footer';
@@ -17,7 +18,7 @@ import GymOwnerLogin from './components/gym_owner_login';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>    
